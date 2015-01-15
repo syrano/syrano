9300712fc391ce9f39971cdf5575034b9bc7502e
Horacio <horacio@pampanetwork.com>
2015-01-14 21:36:45 -0300

    Agrego nuevos archivos

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Horacio Gutierrez - Diseño Web</title>
+    
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
+    <link href="css/queries.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+  </head>
+  <body>
+    <div id="container">
+        <h1 class="animated fadeInDown">HORACIO GUTIERREZ</h1>
+        <h2 class="animated fadeInDown"><b>HORACIO GUTIERREZ</b></h2>
+        <h3 class="animated fadeIn">Diseñador Web - Programador PHP</h3>
+        <h4>HORACIO GUTIERREZ</h4>
+        <h5>Diseñador Web - Programador PHP</h5>
+
+        <section id="main-menu">
+            <ul>
+                <li>HOME</li>
+                <li>ACERCA DE</li>
+                <li>PORTFOLIO</li>
+                <li>INTERACTIVO</li>
+                <li>PRESUPUESTO</li>
+            </ul>
+        </section>
+
+        <section>
+            <ul>
+                
+                <li>Te interesó lo que viste y queres saber mas de mí? es normal...</li>
+                <li>Algunas muestras de lo que hago</li>
+                <li>Probá mis mas recientes locuras</li>
+                <li>Solicitá tu propio presupuesto a medida</li>
+            </ul>
+        </section>
+    </div>
+  </body>
+</html>
