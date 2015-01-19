24b85f68a2f65f9b8c25bb371df10b3da46bf988
Horacio <horacio@pampanetwork.com>
2015-01-19 13:20:57 -0300

    agrego nuevo concepto de ciencia

--- index.php
+++ index.php
@@ -3,19 +3,54 @@
     <div id="container">
         <header>
             <h1 class="animated fadeInDown">HORACIO GUTIERREZ</h1>
-            <h2 class="animated fadeInDown"><b>HORACIO GUTIERREZ</b></h2>
-            <h3 class="animated fadeInDown">Diseñador Web - Programador PHP</h3>
+            <h2 class="animated fadeInDown">
+                <span class="element"><p>3</p>H</span>
+                <span class="element"><p>13</p>G</span>
+            </h2>
+            <h3 class="animated fadeInDown">Dandole vida a tus diseños</h3>
         </header>
+        <section id="main-content">
+            <div id="nucleo">
+                <div id="one">&#9899;</div>
+                <div id="onex">&#9899;</div>
+                <div id="two">&#9899;</div>
+                <div id="twox">&#9899;</div>
+                <div id="three">&#9899;</div>
+                <div id="four">&#9899;</div>
+                <div id="five">&#9899;</div>
+                <div id="six">&#9899;</div>
+                <div id="threex">&#9899;</div>
+                <div id="fourx">&#9899;</div>
+                <div id="fivex">&#9899;</div>
+                <div id="sixx">&#9899;</div>
+                <div id="seven">&#9899;</div>
+                <div id="sevenx">&#9899;</div>
+                <div id="eight">&#9899;</div>
+                <div id="eightx">&#9899;</div>
+                <div id="nine">&#9899;</div>
+                <div id="ninex">&#9899;</div>
+                <div id="ten">&#9899;</div>
+                <div id="tenx">&#9899;</div>
+                <div id="eleven">&#9899;</div>
+                <div id="elevenx">&#9899;</div>
+                <div id="twelve">&#9899;</div>
+                <div id="twelvex">&#9899;</div>
+                <div id="thirt">&#9899;</div>
+                <div id="thirtx">&#9899;</div>
+                <div id="fourt">&#9899;</div>
+                <div id="fourtx">&#9899;</div>
+            </div>
+        </section>
         <section id="main-menu">
             <nav>
-                <a  href="index.php">HOME</a>
-                <a class="menu-link" href="#">ACERCA</a>
-                <a class="menu-link" href="#">PORTFOLIO</a>
-                <a class="menu-link" href="#">TRY-ME</a>
-                <a class="menu-link" href="#">CONTACTO</a>
+                <a href="index.php"><span>HOME</span></a>
+                <a class="menu-link" href="#"><span>ACERCA</span></a>
+                <a class="menu-link" href="#"><span>PORTFOLIO</span></a>
+                <a class="menu-link" href="#"><span>TRY-ME</span></a>
+                <a class="menu-link" href="#"><span>CONTACTO</span></a>
             </nav>
         </section>
-        <section id="content">
+        <!--<section id="content">
             <article>
                 <header>
                     <h2></h2>
@@ -23,7 +58,8 @@
                 <p></p>
                 <div id="switch-result"></div>
             </article>
-        </section>
+        </section>-->
+
     </div>
     <script type="text/javascript">
 
